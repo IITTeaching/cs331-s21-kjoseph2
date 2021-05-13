4f0ab58c4a86e97362615771f21acc18f4b46311
lkevin2018 <44473945+lkevin2018@users.noreply.github.com>
2021-05-13 12:25:59 -0500

    Update radix-sort.py

--- project/radix-sort.py
+++ project/radix-sort.py
@@ -7,4 +7,33 @@ def book_to_words(book_url='https://www.gutenberg.org/files/84/84-0.txt'):
     return bookascii.split()
 
 def radix_a_book(book_url='https://www.gutenberg.org/files/84/84-0.txt'):
-    pass
+    a1 = book_to_words(book_url)
+    mcapacity = len(max(a1, key=len))
+    
+    for x in range(mcapacity):
+        a1 = presort(a1, x, mcapacity - 1)
+    
+    return a1
+
+def presort(book, idx, max):
+    ret = [0 for x in range(len(book))]
+    count = [0 for x in range(128)]
+
+    for word in book:
+        if max - idx < len(word):
+            count[word[max - idx]] += 1
+        else:
+            count[0] += 1
+    
+    for x in range(128):
+        count[x] += count[x-1]
+
+    for x in range(len(book) -1, -1, -1):
+        if max - idx < len(book[x]):
+            ret[count[book[x][max - idx]] - 1] = book[x]
+            count[book[x][max - idx]] -= 1
+        else:
+            ret[count[0] - 1] = book[x]
+            count[0] -= 1
+    
+    return ret
